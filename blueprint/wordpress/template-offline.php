7436c02efe2095db7add1fb6f711de13d0292af7
Mainul Hasan <moh.mainul.hasan@gmail.com>
2022-06-13 16:20:03 +0600

    Template for offline page

---
+++ blueprint/wordpress/template-offline.php
@@ -0,0 +1,13 @@
+<?php /* Template Name: Offline Template */ ?>
+<?php get_header(); ?>
+<div id="primary" class="content-area">
+    <div id="content" class="page-content row" role="main">
+        <div class="page-wrapper">
+            <div class="page-content">
+                <h2>We're sorry, this page hasn't been cached yet :)</h2>
+                <h4 class="offline-link">But why don't you try one of our <a href='/'>other pages?</a></h4>
+            </div><!-- .page-content -->
+        </div><!-- .page-wrapper -->
+    </div><!-- #content -->
+</div><!-- #primary -->
+<?php get_footer(); ?>
