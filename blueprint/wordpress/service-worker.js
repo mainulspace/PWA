b560ffb6189b3d8ea49f8f2c6e799a4e3a9afd6c
Mainul Hasan <moh.mainul.hasan@gmail.com>
2022-06-16 12:43:51 +0600

    Update service-worker functionality for wordpress

--- blueprint/wordpress/service-worker.js
+++ blueprint/wordpress/service-worker.js
@@ -1,14 +1,8 @@
-// Update cache version from service worker URL
-var CACHE_VERSION = new URL(location).searchParams.get('VER');
-var CACHE_STATIC_NAME = 'static-v' + CACHE_VERSION;
-var CACHE_DYNAMIC_NAME = 'dynamic-v' + CACHE_VERSION;
-
-// Update cache version by daily using date
 var today = new Date();
 var version = today.getDate();
+console.log('Today day is : ' + version);
 var CACHE_STATIC_NAME = 'static-v' + version;
 var CACHE_DYNAMIC_NAME = 'dynamic-v' + version;
-
 var EXCLUDE_URL_CACHING = ['/amp', '/wp-includes', '/wp-admin', '/manifest.json', '/robots.txt', '/wp-login.php', '/service-worker.js', '/.well-known/*'];
 var arrayLength = EXCLUDE_URL_CACHING.length;
 var STATIC_FILES = [
@@ -20,10 +14,10 @@ self.addEventListener('install', function (event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
     event.waitUntil(
         caches.open(CACHE_STATIC_NAME)
-        .then(function (cache) {
-            console.log('[Service Worker] Precaching App Shell');
-            cache.addAll(STATIC_FILES);
-        })
+            .then(function (cache) {
+                console.log('[Service Worker] Precaching App Shell');
+                cache.addAll(STATIC_FILES);
+            })
     )
 });
 
@@ -31,54 +25,60 @@ self.addEventListener('activate', function (event) {
     console.log('[Service Worker] Activating Service Worker ....', event);
     event.waitUntil(
         caches.keys()
-        .then(function (keyList) {
-            return Promise.all(keyList.map(function (key) {
-                if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-                    console.log('[Service Worker] Removing old cache.', key);
-                    return caches.delete(key);
-                }
-            }));
-        })
+            .then(function (keyList) {
+                return Promise.all(keyList.map(function (key) {
+                    if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+                        console.log('[Service Worker] Removing old cache.', key);
+                        return caches.delete(key);
+                    }
+                }));
+            })
     );
     return self.clients.claim();
 });
 
-// cache then network strategy
-self.addEventListener('fetch', function (event) {
 
+self.addEventListener('fetch', function(event) {
     if (!(event.request.url.indexOf('http') === 0)) {
         return;
     }
-
     // Doesn't cache following url
     for (var i = 0; i < arrayLength; i++) {
         if (event.request.url.indexOf(EXCLUDE_URL_CACHING[i]) !== -1) {
             return;
         }
     }
-
     if (event.request.method === 'POST' || event.request.method === 'PUT' || event.request.method === 'PATCH' || event.request.method === 'DELETE') {
         return;
     }
-
+    console.log('Fetch from app');
     event.respondWith(
-        caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
-            return cache.match(event.request).then(function (response) {
-                return response || fetch(event.request).then(function (response) {
-                    // Check if we received a valid response
-                    if (!response || response.status !== 200 || response.type !== 'basic') {
+        caches.match(event.request)
+            .then(function(response) {
+                // Cache hit - return response
+                if (response) {
+                    console.log('From cache: ' + response);
+                    return response;
+                }
+                return fetch(event.request).then(
+                    function(response) {
+                        console.log('From network:' + response.status);
+                        // IMPORTANT: Clone the response. A response is a stream
+                        // and because we want the browser to consume the response
+                        // as well as the cache consuming the response, we need
+                        // to clone it so we have two streams.
+                        var responseToCache = response.clone();
+                        caches.open(CACHE_DYNAMIC_NAME)
+                            .then(function(cache) {
+                                console.info('Cached URL:', event.request.url);
+                                cache.put(event.request, responseToCache);
+                            });
                         return response;
                     }
-                    cache.put(event.request, response.clone());
-                    return response;
-                });
-            });
-        }).catch(function (err) {
+                );
+            }).catch(function() {
             // If both fail, show a generic fallback:
-            return caches.open(CACHE_STATIC_NAME)
-            .then(function (cache) {
-                return cache.match('/offline/');
-            })
+            return caches.match('/offline/');
         })
     );
 });
